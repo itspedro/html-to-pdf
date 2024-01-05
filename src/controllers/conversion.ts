edd756dd11ad33c49991ff466aa7f6e2893fb000
Pedro Reis <itspdro@outlook.com>
2024-01-04 21:38:46 -0300

    fix: minor types fix

--- src/controllers/conversion.ts
+++ src/controllers/conversion.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import htmlToPdf from '../helpers/html-to-pdf';
 const router = express.Router();
 
-router.post('/', async (req: any, res: any) => {
+router.post('/', async (req: express.Request, res: express.Response) => {
   const { html } = req.body;
 
   if (!html) {
